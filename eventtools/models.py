80f6b104b2f661ab8ac98fcc41d3913b7dc0fa9b
Greg Brown <greg@gregbrown.co.nz>
2020-11-11 09:42:09 +1300

    Update models.py

--- eventtools/models.py
+++ eventtools/models.py
@@ -349,7 +349,7 @@ class BaseOccurrence(BaseModel):
         db_index=True, null=True, blank=True, verbose_name=_('end'))
 
     repeat = ChoiceTextField(
-        choices=REPEAT_CHOICES, default='', blank=True, 
+        choices=REPEAT_CHOICES, default='', blank=True,
         verbose_name=_('repeat'))
     repeat_until = models.DateField(
         null=True, blank=True, verbose_name=_('repeat_until'))
